9e68763fffc8d357aaac694a6027fb344c55627b
Kevin <54258753+vCiKv@users.noreply.github.com>
2026-02-28 15:11:45 +0100

    Enhance FieldError with code and suggestion properties
    Added optional 'code' and 'suggestion' properties to FieldError.

--- types/errors/FieldError.d.ts
+++ types/errors/FieldError.d.ts
@@ -3,9 +3,16 @@ export declare interface IFieldError {
    * Field of the request that the error describes
    */
   field: string;
-
   /**
    * Human readable description of the problem
    */
   message: string;
+  /**
+   * Error code of the request
+   */
+  code?: string;
+  /**
+   * possible error fix if available 
+   */
+  suggestion?: string;
 }
